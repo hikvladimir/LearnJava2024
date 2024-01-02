24ba6ace0a5a2f2190a77f067a73d2ae4a57275b
hikvladimir <psihouse@mail.ru>
2024-01-02 21:21:07 +0300

    day1

---
+++ day1/lesson1.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("             РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                            О. Хайям");
+    }
+// https://disk.yandex.ru/d/IYbZzjOdey4Idg/_TREGULOV/01%20-%20Java%20%D0%B4%D0%BB%D1%8F%20%D0%BD%D0%B0%D1%87%D0%B8%D0%BD%D0%B0%D1%8E%D1%89%D0%B8%D1%85
+}
